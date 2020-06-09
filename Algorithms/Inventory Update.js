c425137dbf9e479a277ac9880b2539cf73b93550
StaxGit <lario.free@gmail.com>
2020-06-09 06:43:11 +0200

    Solved Inventory Update

--- Algorithms/Inventory Update.js
+++ Algorithms/Inventory Update.js
@@ -30,6 +30,7 @@ updateInventory(curInv, newInv);
 
 
 
+//Version 1 - Doesn't sort the result alphabetically
 
 function updateInventory(arr1, arr2) {
   let existingItems = arr1.map(item => item[1]);
@@ -46,17 +47,83 @@ function updateInventory(arr1, arr2) {
 }
 // Example inventory lists
 var curInv = [
+  [21, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [1, "Hair Pin"],
+  [5, "Microphone"]
+];
+
+var newInv = [
+  [2, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [67, "Bowling Ball"],
+  [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
+
+
+updateInventory(
+  [
     [21, "Bowling Ball"],
     [2, "Dirty Sock"],
     [1, "Hair Pin"],
     [5, "Microphone"]
-];
-
-var newInv = [
+  ],
+  [
     [2, "Hair Pin"],
     [3, "Half-Eaten Apple"],
     [67, "Bowling Ball"],
     [7, "Toothpaste"]
+  ]
+)
+
+[
+  [88, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [3, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [5, "Microphone"],
+  [7, "Toothpaste"]
+]
+
+
+
+// Version 2 - Sorts the result alphabetically!
+
+function updateInventory(arr1, arr2) {
+  let existingItems = arr1.map(item => item[1]);
+  let result = [...arr1];
+  arr2.forEach((item, index) => {
+    if (existingItems.includes(item[1])) {
+      result[existingItems.indexOf(item[1])][0] += arr2[index][0];
+      arr2.splice(index, 1);
+    }
+  })
+  result = result.concat(arr2);
+  result.sort(
+    function (a, b) {
+      if (a[1] < b[1]) { return -1; }
+      if (a[1] > b[1]) { return 1; }
+      return 0;
+    })
+  console.log(result);
+  // All inventory must be accounted for or you're fired!
+  return result;
+}
+// Example inventory lists
+var curInv = [
+  [21, "Bowling Ball"],
+  [2, "Dirty Sock"],
+  [1, "Hair Pin"],
+  [5, "Microphone"]
+];
+
+var newInv = [
+  [2, "Hair Pin"],
+  [3, "Half-Eaten Apple"],
+  [67, "Bowling Ball"],
+  [7, "Toothpaste"]
 ];
 
 updateInventory(curInv, newInv);
