47d6297a451aa3d825e65ce91569b5ca2ef9bef7
StaxGit <lario.free@gmail.com>
2020-06-09 06:35:53 +0200

    Inventory Update - V1 - Result not in alphabetical order

--- Algorithms/Inventory Update.js
+++ Algorithms/Inventory Update.js
@@ -27,3 +27,36 @@ var newInv = [
 ];
 
 updateInventory(curInv, newInv);
+
+
+
+
+function updateInventory(arr1, arr2) {
+  let existingItems = arr1.map(item => item[1]);
+  let result = [...arr1];
+  arr2.forEach((item, index) => {
+    if (existingItems.includes(item[1])) {
+      result[existingItems.indexOf(item[1])][0] += arr2[index][0];
+      arr2.splice(index, 1);
+    }
+  })
+  result = result.concat(arr2);
+  // All inventory must be accounted for or you're fired!
+  return result;
+}
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
