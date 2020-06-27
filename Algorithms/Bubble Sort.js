f12b9fc73a6c3fcab1622ed8cd30693b79afb00e
StaxGit <lario.free@gmail.com>
2020-06-27 21:14:42 +0200

    Solved Bubble Sort

--- Algorithms/Bubble Sort.js
+++ Algorithms/Bubble Sort.js
@@ -29,3 +29,27 @@ function bubbleSort(array) {
 }
 
 bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+
+
+function bubbleSort(array) {
+  // change code below this line
+  let result = [...array]
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] > result[i + 1]) {
+        let temp = result[i];
+        result[i] = result[i + 1];
+        result[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped === true);
+
+  console.log(result)
+  return result;
+  // change code above this line
+}
+
+bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
