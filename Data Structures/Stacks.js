786f254ede6154aa01e564cc25882666189d211b
StaxGit <lario.free@gmail.com>
2020-07-14 17:47:07 +0200

    Solved Stacks

--- Data Structures/Stacks.js
+++ Data Structures/Stacks.js
@@ -32,3 +32,8 @@ var homeworkStack = ["BIO12","HIS80","MAT122","PSY44"];
 // Only change code below this line
 
 
+var homeworkStack = ["BIO12","HIS80","MAT122","PSY44"];
+// Only change code below this line
+
+homeworkStack.pop();
+homeworkStack.push("CS50");
