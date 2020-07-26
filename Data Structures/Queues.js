8b306b7f20a927aa4675cac23c51532b23bac7c2
StaxGit <lario.free@gmail.com>
2020-07-26 22:16:20 +0200

    Solved Circular Queues

--- Data Structures/Queues.js
+++ Data Structures/Queues.js
@@ -110,9 +110,9 @@ function PriorityQueue() {
   this.enqueue = function (input) {
     let inputPriority = input[1];
     let correctIndex;
-    this.collection.forEach( function (element, index) {
+    this.collection.forEach(function (element, index) {
       if (element[1] <= inputPriority) {
-        correctIndex = index+1;
+        correctIndex = index + 1;
       }
     })
     this.collection.splice(correctIndex, 0, input);
@@ -214,6 +214,59 @@ class CircularQueue {
   dequeue() {
     // Only change code below this line
 
+    // Only change code above this line
+  }
+}
+
+
+class CircularQueue {
+  constructor(size) {
+
+    this.queue = [];
+    this.read = 0;
+    this.write = 0;
+    this.max = size - 1;
+
+    while (size > 0) {
+      this.queue.push(null);
+      size--;
+    }
+  }
+
+  print() {
+    return this.queue;
+  }
+
+  enqueue(item) {
+    // Only change code below this line
+    if (this.queue[this.write] == null) {
+
+      this.queue[this.write] = item;
+      if (this.write < this.max) {
+        this.write++;
+      } else {
+        this.write = 0;
+      }
+      return item;
+    }
+    return null;
+
+    // Only change code above this line
+  }
+
+  dequeue() {
+    // Only change code below this line
+    if (this.queue[this.read] != null) {
+      let item = this.queue[this.read];
+      this.queue[this.read] = null;
+      if (this.read < this.max) {
+        this.read++;
+      } else {
+        this.read = 0;
+      }
+      return item;
+    }
+    return null;
     // Only change code above this line
   }
 }
