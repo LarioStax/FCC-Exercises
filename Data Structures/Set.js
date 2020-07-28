7e27803b211baec13da9c1af2881a403b86fafca
StaxGit <lario.free@gmail.com>
2020-07-28 10:50:19 +0200

    Solved Join Two Sets

--- Data Structures/Set.js
+++ Data Structures/Set.js
@@ -153,3 +153,56 @@ class Set {
 
   // change code above this line
 }
+
+
+
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // this method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // this method will return all the values in the set
+  values() {
+    return Object.keys(this.dictionary);
+  }
+  // this method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = true;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // change code below this line
+  union(newSet) {
+    let unionSet = new Set();
+    console.log(this);
+    let firstValues = this.values();
+    let secondValues = newSet.values();
+    firstValues.forEach(val => unionSet.add(val));
+    secondValues.forEach(val => unionSet.add(val));
+    return unionSet;
+  }
+  // change code above this line
+};
