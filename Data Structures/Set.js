c5f5a7c4aa2fcde392a0ad89b971daab38f72072
StaxGit <lario.free@gmail.com>
2020-07-29 22:28:34 +0200

    Sets Intersection Setup

--- Data Structures/Set.js
+++ Data Structures/Set.js
@@ -206,3 +206,68 @@ class Set {
   }
   // change code above this line
 };
+
+
+// Data Structures: Perform an Intersection on Two Sets of Data
+
+// In this exercise we are going to perform an intersection on 2 sets of data. We will 
+// create a method on our Set data structure called intersection. An intersection of 
+// sets represents all values that are common to two or more sets. This method should 
+// take another Set as an argument and return the intersection of the two sets.
+
+// For example, if setA = ['a','b','c'] and setB = ['a','b','d','e'], then the 
+// intersection of setA and setB is: setA.intersection(setB) = ['a', 'b'].
+
+class Set {
+  constructor() {
+    // This will hold the set
+    this.dictionary = {};
+    this.length = 0;
+  }
+  // this method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+  // this method will return all the values in the set
+  values() {
+    return Object.keys(this.dictionary);
+  }
+  // this method will add an element to the set
+  add(element) {
+    if (!this.has(element)) {
+      this.dictionary[element] = true;
+      this.length++;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will remove an element from a set
+  remove(element) {
+    if (this.has(element)) {
+      delete this.dictionary[element];
+      this.length--;
+      return true;
+    }
+
+    return false;
+  }
+  // this method will return the size of the set
+  size() {
+    return this.length;
+  }
+  // This is our union method from that lesson
+  union(set) {
+    const newSet = new Set();
+    this.values().forEach(value => {
+      newSet.add(value);
+    })
+    set.values().forEach(value => {
+      newSet.add(value);
+    })
+
+    return newSet;
+  }
+  // change code below this line
+  // change code above this line
+}
