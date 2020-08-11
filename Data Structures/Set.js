5ba0e9654930be562183b6ff70ed7158c7cf769b
StaxGit <lario.free@gmail.com>
2020-08-12 01:09:27 +0200

    Solved Create and Add Sets

--- Data Structures/Set.js
+++ Data Structures/Set.js
@@ -658,3 +658,14 @@ function checkSet() {
 }
 
 checkSet();
+
+function checkSet() {
+  var set = new Set([1, 2, 3, 3, 2, 1, 2, 3, 1]);
+  // change code below this line
+  set.add("Taco").add("Cat").add("Awesome");
+  // change code above this line
+  console.log(Array.from(set));
+  return set;
+}
+
+checkSet();
