b795d3f16152eebcccfed916634dddcabc0ce187
StaxGit <lario.free@gmail.com>
2020-08-13 23:50:13 +0200

    Setup Remove Items from Sets

--- Data Structures/Set.js
+++ Data Structures/Set.js
@@ -669,3 +669,29 @@ function checkSet() {
 }
 
 checkSet();
+
+
+// Data Structures: Remove items from a set in ES6
+
+// Let's practice removing items from an ES6 Set using the delete method.
+
+// First, create an ES6 Set:
+
+// var set = new Set([1,2,3]);
+
+// Now remove an item from your Set with the delete method.
+
+// set.delete(1);
+// console.log([...set]) // should return [ 2, 3 ]
+
+// Now, create a set with the integers 1, 2, 3, 4, & 5.
+
+// Remove the values 2 and 5, and then return the set.
+
+function checkSet(){
+  var set = //Create a set with values 1, 2, 3, 4, & 5
+  //Remove the value 2
+  //Remove the value 5
+  //Return the set
+  return set;
+}
