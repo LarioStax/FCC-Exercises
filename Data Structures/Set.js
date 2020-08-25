ca6d31605a7611970850a8826fdf7f778989ee9b
StaxGit <lario.free@gmail.com>
2020-08-25 23:25:51 +0200

    Solved Set Remove Items

--- Data Structures/Set.js
+++ Data Structures/Set.js
@@ -697,3 +697,14 @@ function checkSet(){
   return set;
 }
  
+
+function checkSet(){
+  var set = new Set([1,2,3,4,5]);
+  set.delete(2);
+  set.delete(5);
+  //Remove the value 2
+  //Remove the value 5
+  //Return the set
+  return set;
+}
+
